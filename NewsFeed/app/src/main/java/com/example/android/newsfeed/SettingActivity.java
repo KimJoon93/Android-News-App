460b7aab26fa63a4aae1ebf4f911d8480fb38614
KimJoon93 <jk1094@naver.com>
2019-03-01 23:52:43 +0900

    Setting finished

--- NewsFeed/app/src/main/java/com/example/android/newsfeed/SettingActivity.java
+++ NewsFeed/app/src/main/java/com/example/android/newsfeed/SettingActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.newsfeed;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        ActionBar actionBar = this.getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
